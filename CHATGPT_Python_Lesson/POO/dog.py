34337f038f8b8281fdb3fec0b0b68fed559456a2
thygolem <thygolem@gmail.com>
2023-01-31 13:19:08 +0100

    Herencias
    En este ejemplo, la clase Dog hereda de la clase Animal. La clase Animal tiene un método make_sound que imprime un sonido genérico de un animal. La clase Dog sobrescribe este método con su propia implementación que imprime "Woof!" en lugar de un sonido genérico de animal.
    
    Además, la clase Dog llama al método __init__ de la clase padre con super().__init__("Canis lupus familiaris") para establecer el valor de species en la clase Animal.

--- CHATGPT_Python_Lesson/POO/dog.py
+++ CHATGPT_Python_Lesson/POO/dog.py
@@ -1,7 +1,18 @@
-class Dog:
+class Animal:
+    def __init__(self, species):
+        self.species = species
+
+    def make_sound(self):
+        print("Some generic animal sound")
+
+class Dog(Animal):
     def __init__(self, name, breed):
+        super().__init__("Canis lupus familiaris")
         self.name = name
         self.breed = breed
 
     def bark(self):
+        print("Woof!")
+
+    def make_sound(self):
         print("Woof!")
