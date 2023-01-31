722af065d0b203b9e64d26459ece5ec855a7898b
thygolem <thygolem@gmail.com>
2023-01-31 13:25:08 +0100

    animal classes

---
+++ CHATGPT_Python_Lesson/POO/animalClass.py
@@ -0,0 +1,24 @@
+class Animal:
+    def __init__(self, species):
+        self.species = species
+
+    def make_sound(self):
+        print("Some generic animal sound")
+
+class Dog(Animal):
+    def __init__(self, name, breed):
+        super().__init__("Canis lupus familiaris")
+        self.name = name
+        self.breed = breed
+
+    def bark(self):
+        print("Woof!")
+
+    def make_sound(self):
+        print("Woof!")
+
+animals = [Animal("Lion"), Animal("Tiger"), Dog("Fido", "Golden Retriever")]
+
+for animal in animals:
+    print(animal.species)
+    animal.make_sound()
